567c99d07581ca89e56a59b9284dd2180f6ef2f0
sayonil-mitra <sayonil.m@mobiusdtaas.ai>
2024-11-14 12:09:57 +0530

    Initial Commit

---
+++ constants/constant.js
@@ -0,0 +1,7 @@
+export const CONSTANTS = {
+	BaseUrl: 'https://ig.gov-cloud.ai',
+	BaseUrlOld: 'https://ig.aidtaas.com',
+	EndpointAdhoc: 'pi-cohorts-service/v1.0/cohorts/adhoc',
+	EndPointCreateBQ: 'pi-bigquery-service/v1.0/big-queries',
+	EndPointGetBqData: 'pi-get-data-quarkus/v1.0/bigQuery',
+};
